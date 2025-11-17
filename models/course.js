2292a28722bdf8502e664fea6f8e416647ddca51
Ankush Gupta <iamankushgupta68@gmail.com>
2025-11-17 22:47:44 +0530

    NEW

--- models/course.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const Course = sequelize.define('Course', {
-    title: { type: DataTypes.STRING, allowNull: false },
-    description: { type: DataTypes.TEXT },
-    duration: { type: DataTypes.STRING },
-    price: { type: DataTypes.FLOAT }
-  });
-  return Course;
-};
